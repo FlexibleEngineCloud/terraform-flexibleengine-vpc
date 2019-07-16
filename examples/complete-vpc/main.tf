56cb2a6ecee07139f9a4608ee551ea9738fc4bc0
Antonin <antonin.anchisi@orange.com>
2019-07-16 11:39:25 +0200

    Create main.tf

---
+++ examples/complete-vpc/main.tf
@@ -0,0 +1,30 @@
+module "vpc"{
+  source ="../.."
+
+  vpc_name ="vpc_example"
+
+  vpc_cidr ="10.0.0.0/16"
+
+  vpc_subnets= [
+      {
+        subnet_name       = "subnet-0"
+        subnet_cidr       = "10.0.1.0/24"
+        subnet_gateway_ip = "10.0.1.1"
+      },
+      {
+        subnet_name       = "subnet-1"
+        subnet_cidr       = "10.0.2.0/24"
+        subnet_gateway_ip = "10.0.2.1"
+      },
+  ]
+
+  vpc_snat_subnets = ["subnet-0"]
+
+  enable_nat_gateway = true
+
+  nat_gateway_name = "natg-0"
+
+  nat_gateway_type = "1"
+
+  nat_gateway_subnet_name = "subnet-0"
+}
