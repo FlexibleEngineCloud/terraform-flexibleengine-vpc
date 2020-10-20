00139dc73c876bdc7a91b4542473d3660503c785
olivier <olivier.saluden@orange.com>
2020-10-20 18:35:13 +0200

    Revert subnet map key change

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "flexibleengine_vpc_subnet_v1" "vpc_subnets" {
   # Create subnets
   for_each = local.vpc_subnets_map
   
-  name          = each.value.subnet_name
-  cidr          = each.key
+  name          = each.key
+  cidr          = each.value.subnet_cidr
   gateway_ip    = each.value.subnet_gateway_ip
   primary_dns   = var.primary_dns
   secondary_dns = var.secondary_dns
@@ -42,7 +42,7 @@ resource "flexibleengine_nat_gateway_v2" "nat_gateway" {
 
 locals {
  
-  vpc_subnets_keys = [for subnet in var.vpc_subnets : subnet.subnet_cidr]
+  vpc_subnets_keys = [for subnet in var.vpc_subnets : subnet.subnet_name]
   vpc_subnets_values = [for subnet in var.vpc_subnets : subnet]
   vpc_subnets_map = zipmap(local.vpc_subnets_keys, local.vpc_subnets_values)
 
@@ -69,6 +69,6 @@ resource "flexibleengine_nat_snat_rule_v2" "snat" {
   for_each = local.vpc_snat_subnets_map_network_id
   
   nat_gateway_id = flexibleengine_nat_gateway_v2.nat_gateway[0].id
-  network_id=flexibleengine_vpc_subnet_v1.vpc_subnets[each.value.subnet_name].id
+  network_id=flexibleengine_vpc_subnet_v1.vpc_subnets[each.value].id
   floating_ip_id = var.new_eip ? flexibleengine_vpc_eip_v1.new_eip[0].id : var.existing_eip_id
 }
